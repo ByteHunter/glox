def45033a097a364d480f1e411b642c48732f05e
ByteHunter <info@bytehunter.net>
2026-02-26 18:41:51 +0000

    Implementing basic logic to consume the source code, with tests

--- scanner.go
+++ scanner.go
@@ -1,12 +1,54 @@
 package main
 
 type Scanner struct {
+	source               string
+	tokens               []Token
+	start, current, line int
 }
 
 func NewScanner(source string) *Scanner {
-	return &Scanner{}
+	return &Scanner{
+		source:  source,
+		start:   0,
+		current: 0,
+		line:    1,
+	}
 }
 
-func (scanner *Scanner) scanTokens() error {
-	return nil
+func (s *Scanner) scanTokens() ([]Token, error) {
+	for !s.isAtEnd() {
+		s.start = s.current
+		s.scanToken()
+	}
+	s.addSimpleToken(EOF)
+	return s.tokens, nil
+}
+
+func (s *Scanner) scanToken() {
+	b := s.advance()
+
+	switch b {
+	case '(':
+	}
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.current >= len(s.source)
+}
+
+func (s *Scanner) advance() byte {
+	b := s.source[s.current]
+	s.current++
+
+	return b
+}
+
+func (s *Scanner) addSimpleToken(tokenType TokenType) {
+	s.addToken(tokenType, nil)
+}
+
+func (s *Scanner) addToken(tokenType TokenType, value any) {
+	lexeme := s.source[s.start:s.current]
+	newToken := NewToken(tokenType, lexeme, value, s.line)
+	s.tokens = append(s.tokens, *newToken)
 }
--- scanner_test.go
+++ scanner_test.go
@@ -3,16 +3,76 @@ package main
 import "testing"
 
 func TestNewScanner(t *testing.T) {
-	ans := NewScanner("")
-	if ans == nil {
-		t.Errorf("NewScanner() is nil")
+	scanner := NewScanner("")
+	if scanner == nil {
+		t.Errorf("Expected to be not nil")
+	}
+	var tests = []struct {
+		name             string
+		actual, expected any
+	}{
+		{"source", scanner.source, ""},
+		{"start", scanner.start, 0},
+		{"current", scanner.current, 0},
+		{"line", scanner.line, 1},
+	}
+	for _, test := range tests {
+		if test.actual != test.expected {
+			t.Errorf(
+				"Expected '%s' to be '%v' but got '%v'",
+				test.name, test.expected, test.actual,
+			)
+		}
 	}
 }
 
 func TestScanTokensWithoutErrors(t *testing.T) {
 	scanner := NewScanner("")
-	err := scanner.scanTokens()
+	tokens, err := scanner.scanTokens()
 	if err != nil {
-		t.Errorf("Scanner.scanTokens() want nil but got: %s", err.Error())
+		t.Errorf("Expected '%v' but got '%v'", nil, err.Error())
+	}
+	actual := len(tokens)
+	expected := 1
+	if actual != expected {
+		t.Errorf("Expected length '%v' but got '%v'", expected, actual)
+	}
+}
+
+func TestIsAtEnd(t *testing.T) {
+	scanner := NewScanner("")
+	actual := scanner.isAtEnd()
+	expected := true
+	if actual != expected {
+		t.Errorf("Expected to be '%v' but got '%v'", expected, actual)
+	}
+}
+
+func TestAdvanceWithSource(t *testing.T) {
+	scanner := NewScanner("(")
+	actual := scanner.advance()
+	expected := byte('(')
+	if actual != expected {
+		t.Errorf("Expected '%v' but got '%v'", expected, actual)
+	}
+}
+
+func TestAddSimpleToken(t *testing.T) {
+	scanner := NewScanner("")
+	scanner.addSimpleToken(EOF)
+	actual := len(scanner.tokens)
+	expected := 1
+	if actual != expected {
+		t.Errorf("Expected '%v' but got '%v'", expected, actual)
+	}
+}
+
+func TestAddToken(t *testing.T) {
+	scanner := NewScanner("")
+	scanner.addToken(EOF, nil)
+	actual := len(scanner.tokens)
+	expected := 1
+	if actual != expected {
+		t.Errorf("Expected '%v' but got '%v'", expected, actual)
 	}
 }
