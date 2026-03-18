ee218699ed877e853a3e30ac29a375bc2fdbb669
ByteHunter <info@bytehunter.net>
2026-03-18 17:34:43 +0000

    Completing runFile tests and moving main.lox to test resources

--- main_test.go
+++ main_test.go
@@ -5,9 +5,21 @@ import (
 )
 
 func TestRunExistingFileWithoutErrors(t *testing.T) {
-	err := runFile("main.lox")
+	err := runFile("./tests/resources/main.lox")
 	if err != nil {
 		t.Errorf("runFile() want nil but got: %s", err)
+	}
+}
+
+func TestRunFileWithErrors(t *testing.T) {
+	err := runFile("")
+	if err == nil {
+		t.Errorf("Expected an error but got nil")
+	}
+	actual := err.Error()
+	expected := "open : no such file or directory"
+	if actual != expected {
+		t.Errorf("Expected length '%v' but got '%v'", expected, actual)
 	}
 }
 
