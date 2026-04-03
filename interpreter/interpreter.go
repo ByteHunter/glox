e8477d9c23c3f02c6758676960cf1d557e0ad368
ByteHunter <rost18mlg@gmail.com>
2026-04-03 11:38:44 +0100

    We start by evaluating literals and unary, with helper methods for conversion

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,7 +1,66 @@
 package interpreter
 
-type Interpreter struct {}
+import (
+	"errors"
+	"math"
+
+	"github.com/ByteHunter/glox/expression"
+	"github.com/ByteHunter/glox/reporting"
+	"github.com/ByteHunter/glox/token"
+)
+
+type Interpreter struct{}
 
 func NewInterpreter() *Interpreter {
 	return &Interpreter{}
+}
+
+func (i *Interpreter) VisitBinaryExpression(expr *expression.Binary) any {
+	return nil
+}
+
+func (i *Interpreter) VisitGroupingExpression(expr *expression.Grouping) any {
+	return i.Evaluate(expr.Expr)
+}
+
+func (i *Interpreter) VisitLiteralExpression(expr *expression.Literal) any {
+	return expr.Value
+}
+
+func (i *Interpreter) VisitUnaryExpression(expr *expression.Unary) any {
+	if expr.Right == nil {
+		reporting.LoxError(expr.Operator.Line, "Expected an expression, nil found (InterpreterError)")
+		return nil
+	}
+	right := i.Evaluate(expr.Right)
+
+	switch expr.Operator.Type {
+	case token.BANG:
+		return ! i.getBoolean(right)
+	case token.MINUS:
+		res, _ := i.getFloat(right)
+		return -res
+	}
+
+	return nil
+}
+
+func (i *Interpreter) Evaluate(expr expression.Expression) any {
+	return expr.Accept(i)
+}
+
+func (i *Interpreter) getFloat(v any) (float64, error) {
+	switch t := v.(type) {
+	case int:
+		return float64(t), nil
+	default:
+		return math.NaN(), errors.New("Cannot convert to float64, unexpected type")
+	}
+}
+
+func (i *Interpreter) getBoolean(v any) bool {
+	if v == nil || v == false {
+		return false
+	}
+	return true
 }
--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -1,1 +1,73 @@
 package interpreter
+
+import (
+	"fmt"
+
+	"github.com/ByteHunter/glox/expression"
+	"github.com/ByteHunter/glox/token"
+)
+
+func ExampleInterpreter_Evaluate_unary_minus() {
+	i := NewInterpreter()
+	expr := expression.NewUnary(
+		*token.NewToken(token.MINUS, "-", nil, 1),
+		expression.NewLiteral(42),
+	)
+	result := i.Evaluate(expr)
+	fmt.Println(result)
+
+	// Output:
+	// -42
+}
+
+func ExampleInterpreter_Evaluate_unary_minus2() {
+	i := NewInterpreter()
+	expr := expression.NewUnary(
+		*token.NewToken(token.MINUS, "-", nil, 1),
+		expression.NewLiteral(-42),
+	)
+	result := i.Evaluate(expr)
+	fmt.Println(result)
+
+	// Output:
+	// 42
+}
+
+func ExampleInterpreter_Evaluate_unary_bang_true() {
+	i := NewInterpreter()
+	expr := expression.NewUnary(
+		*token.NewToken(token.BANG, "!", nil, 1),
+		expression.NewLiteral(true),
+	)
+	result := i.Evaluate(expr)
+	fmt.Println(result)
+
+	// Output:
+	// false
+}
+
+func ExampleInterpreter_Evaluate_unary_bang_false() {
+	i := NewInterpreter()
+	expr := expression.NewUnary(
+		*token.NewToken(token.BANG, "!", nil, 1),
+		expression.NewLiteral(false),
+	)
+	result := i.Evaluate(expr)
+	fmt.Println(result)
+
+	// Output:
+	// true
+}
+
+func ExampleInterpreter_Evaluate_unary_bang_other() {
+	i := NewInterpreter()
+	expr := expression.NewUnary(
+		*token.NewToken(token.BANG, "!", nil, 1),
+		expression.NewLiteral(42),
+	)
+	result := i.Evaluate(expr)
+	fmt.Println(result)
+
+	// Output:
+	// false
+}
